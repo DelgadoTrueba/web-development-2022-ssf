c501d6228bedee50eadd861b70eefc0efdba7681
Jose Manuel Delgado Trueba <jose.delgado@babel.es>
2022-09-13 08:36:54 +0200

    config/initial_setup: add html

--- example1/scripts/webpack.config.dev.js
+++ example1/scripts/webpack.config.dev.js
@@ -1,11 +1,19 @@
 const path = require('path')
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({  
+      filename: 'index.html',
+      template: 'src/index.html',
+    })
+  ],
   devServer: {
     port: 3009
   }
