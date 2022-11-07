3b45cb9887029018da47435a74b5a19514f5204f
Sergi Simon Ferrer <sergi.simon@babelgroup.com>
2022-11-07 12:36:36 +0100

    feat:Exercice3 react

---
+++ ejerciciosReact/ejercicio3/src/components/Expenses.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div className="expenses">
+      <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default Expenses;
